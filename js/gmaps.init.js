e0bfeae8d2ab370c8d67871b255330adb1b0738c
VesAndrei <veselu.andrei@gmail.com>
2016-12-02 13:35:22 +0100

    Map

---
+++ js/gmaps.init.js
@@ -0,0 +1,44 @@
+$(function () {
+
+    map();
+
+});
+
+/* map */
+
+function map() {
+
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    
+	map = new GMaps(
+	{
+	el: '#map',
+	lat: 51.91749633,
+	lng: 4.522164578,
+	zoomControl: true,
+	zoomControlOpt: {
+	    style: 'SMALL',
+	    position: 'TOP_LEFT'
+	},
+	panControl: false,
+	streetViewControl: false,
+	mapTypeControl: false,
+	overviewMapControl: false,
+	scrollwheel: false,
+	draggable: false,
+	styles: styles
+    });
+
+    var image = 'img/marker.png';
+
+    map.addMarker({
+	lat: 51.91749633,
+	lng: 4.522164578,
+	icon: image,
+	title: 'Turing Society',
+	infoWindow: {
+	content: '<p>Turing Society Rotterdam</p>'
+	}
+	
+    });
+}
